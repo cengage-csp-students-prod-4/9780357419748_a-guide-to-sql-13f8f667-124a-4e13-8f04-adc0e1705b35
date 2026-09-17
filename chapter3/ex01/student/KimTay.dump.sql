bdacb07d17a43ec207c1d7ae6b78fe1c245a356b
Makaylamckay9999 <nicolemckay4320@gmail.com>
2026-09-17 22:00:09 +0000

    Testing Thu Sep 17 2026 22:00:09 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex01/student/KimTay.dump.sql
+++ chapter3/ex01/student/KimTay.dump.sql
@@ -205,20 +205,4 @@ UNLOCK TABLES;
 
 -- Dump completed on 2026-09-17 21:00:04
 
-CREATE table REP (
-    REP_ID char(2) PRIMARY KEY,
-    FIRST_NAME varchar(20),
-    LAST_NAME varchar(20),
-    ADDRESS varchar(20),
-    CITY varchar(15),
-    STATE char(2),
-    POSTAL char(5),
-    CELL_PHONE char(12),
-    COMISSION decimal(7,2),
-    RATE decimal(3,2)
-);
-
-INSERT INTO REP (REP_ID, FIRST_NAME, LAST_NAME, ADDRESS, CITY, STATE, POSTAL, CELL_PHONE, COMMISSION, RATE)
-VALUES
-('35','Fred', 'Kiser', '427 Billings Dr', 'Cody', 'WY', '82414', '307-555-6309', 0.00, 0.05);
-
+DESCRIBE REP;
