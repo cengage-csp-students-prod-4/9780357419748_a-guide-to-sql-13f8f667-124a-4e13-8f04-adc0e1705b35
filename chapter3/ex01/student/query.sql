283ccee691a9c0286982e3530b4114aefeeefbb8
Makaylamckay9999 <nicolemckay4320@gmail.com>
2026-09-17 21:05:38 +0000

    Testing Thu Sep 17 2026 21:05:38 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex01/student/query.sql
+++ chapter3/ex01/student/query.sql
@@ -11,3 +11,7 @@ CREATE table Rep (
     Rate decimal(3,2)
 
 );
+--!Task 2
+Insert Into REP (REP_ID, FIRST_NAME, LAST_NAME, ADDRESS, CITY, STATE, POSTAL, CELL_PHONE, COMMISSION, RATE)
+VALUES
+('35', 'Fred', 'Kiser', '427 Billings Dr.', 'Cody', 'WY','82414', '307-555-6309', 0.00, 0.05);
