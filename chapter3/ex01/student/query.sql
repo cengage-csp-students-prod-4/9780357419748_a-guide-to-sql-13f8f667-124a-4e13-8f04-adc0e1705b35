2bce778b0ebcac8abd2e7e09e36f7664c4139e93
Makaylamckay9999 <nicolemckay4320@gmail.com>
2026-09-17 21:00:03 +0000

    Testing Thu Sep 17 2026 21:00:03 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex01/student/query.sql
+++ chapter3/ex01/student/query.sql
@@ -1,1 +1,13 @@
--- Write your query here
+CREATE table Rep (
+    Rep_ID char (2) Primary Key,
+    First_Name varchar(20),
+    Last_Name varchar(20),
+    Address varchar(20),
+    CITY varchar(15),
+    STATE char(2),
+    Postal char(5),
+    Cell_Phone char(12),
+    COMMISSION decimal (7,2),
+    Rate decimal(3,2)
+
+);
